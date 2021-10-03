150a9b4eadce61fa224f424e350898a02e1e191f
James Weber <jweber@weberbox.com>
2021-10-03 13:45:36 -0700

    Update FirebaseUtils on nonfirebase flavor to Timber logging

--- app/src/nonfirebase/java/com/weberbox/pifire/utils/FirebaseUtils.java
+++ app/src/nonfirebase/java/com/weberbox/pifire/utils/FirebaseUtils.java
@@ -2,18 +2,20 @@ package com.weberbox.pifire.utils;
 
 import android.content.Context;
 
+import timber.log.Timber;
+
 public class FirebaseUtils {
     private static final String TAG = FirebaseUtils.class.getSimpleName();
 
     public static void getFirebaseToken(Context context) {
-        Log.d(TAG, "Non Firebase Build");
+        Timber.d("Non Firebase Build");
     }
 
     public static void toggleFirebaseSubscription(boolean subscribe) {
-        Log.d(TAG, "Non Firebase Build");
+        Timber.d("Non Firebase Build");
     }
 
     public static void subscribeFirebase() {
-        Log.d(TAG, "Non Firebase Build");
+        Timber.d("Non Firebase Build");
     }
 }
