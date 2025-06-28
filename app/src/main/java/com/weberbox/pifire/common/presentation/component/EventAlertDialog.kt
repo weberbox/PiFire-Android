22527277664921fd03667442be456238c9331e23
weberbox <313328+weberbox@users.noreply.github.com>
2025-06-28 16:47:51 -0700

    Refactor `EventAlertDialog` to extract `DialogContent` for improved readability and structure. Replace `TextButton` with `OutlinedButton` and `Button` for better UI consistency. Adjust padding and arrangement for dialog buttons.

--- app/src/main/java/com/weberbox/pifire/common/presentation/component/EventAlertDialog.kt
+++ app/src/main/java/com/weberbox/pifire/common/presentation/component/EventAlertDialog.kt
@@ -2,7 +2,9 @@ package com.weberbox.pifire.common.presentation.component
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.displayCutoutPadding
@@ -11,27 +13,32 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.composables.core.Dialog
 import com.composables.core.DialogPanel
 import com.composables.core.DialogProperties
 import com.composables.core.Scrim
-import com.weberbox.pifire.common.presentation.util.asString
+import com.composables.core.rememberDialogState
+import com.weberbox.pifire.R
 import com.weberbox.pifire.common.presentation.state.EventDialogState
-import com.weberbox.pifire.common.presentation.state.rememberEventDialogState
 import com.weberbox.pifire.common.presentation.theme.PiFireTheme
 import com.weberbox.pifire.common.presentation.theme.spacing
+import com.weberbox.pifire.common.presentation.util.DialogAction
+import com.weberbox.pifire.common.presentation.util.DialogEvent
+import com.weberbox.pifire.common.presentation.util.UiText
+import com.weberbox.pifire.common.presentation.util.asString
 import com.weberbox.pifire.common.presentation.util.fadeEnterTransition
 import com.weberbox.pifire.common.presentation.util.fadeExitTransition
 import com.weberbox.pifire.common.presentation.util.scaleEnterTransition
@@ -42,7 +49,6 @@ fun EventAlertDialog(
     modifier: Modifier = Modifier,
     eventDialogState: EventDialogState
 ) {
-    val scope = rememberCoroutineScope()
     Dialog(
         state = eventDialogState.state,
         properties = DialogProperties(
@@ -65,62 +71,78 @@ fun EventAlertDialog(
             enter = scaleEnterTransition(),
             exit = fadeExitTransition(200)
         ) {
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Column(
-                    Modifier.padding(
-                        start = MaterialTheme.spacing.mediumTwo,
-                        top = MaterialTheme.spacing.mediumTwo,
-                        end = MaterialTheme.spacing.mediumTwo
-                    )
+            DialogContent(
+                eventDialogState = eventDialogState
+            )
+        }
+    }
+}
+
+@Composable
+private fun DialogContent(
+    eventDialogState: EventDialogState
+) {
+    val scope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            Modifier.padding(
+                start = MaterialTheme.spacing.mediumTwo,
+                top = MaterialTheme.spacing.mediumTwo,
+                end = MaterialTheme.spacing.mediumTwo
+            )
+        ) {
+            Text(
+                text = eventDialogState.event.title.asString(),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(Modifier.height(MaterialTheme.spacing.small))
+            Text(
+                text = eventDialogState.event.message.asString(),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row(
+            modifier = Modifier
+                .padding(MaterialTheme.spacing.smallTwo)
+                .padding(end = MaterialTheme.spacing.smallOne)
+                .align(Alignment.End),
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.smallOne)
+        ) {
+            eventDialogState.event.negativeAction?.let { event ->
+                OutlinedButton(
+                    contentPadding = PaddingValues(horizontal = MaterialTheme.spacing.smallThree),
+                    onClick = {
+                        eventDialogState.dismiss()
+                        scope.launch {
+                            event.action.invoke()
+                        }
+                    }
                 ) {
                     Text(
-                        text = eventDialogState.event.title.asString(),
-                        style = MaterialTheme.typography.titleMedium,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Spacer(Modifier.height(MaterialTheme.spacing.small))
-                    Text(
-                        text = eventDialogState.event.message.asString(),
-                        style = MaterialTheme.typography.bodyMedium
+                        text = event.buttonText.asString(),
+                        color = MaterialTheme.colorScheme.onSurface
                     )
                 }
-                Row(
-                    modifier = Modifier
-                        .padding(MaterialTheme.spacing.smallTwo)
-                        .align(Alignment.End),
+            }
+            eventDialogState.event.positiveAction.let { event ->
+                Button(
+                    contentPadding = PaddingValues(horizontal = MaterialTheme.spacing.smallThree),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.inverseSurface,
+                    ),
+                    onClick = {
+                        eventDialogState.dismiss()
+                        scope.launch {
+                            event.action.invoke()
+                        }
+                    },
                 ) {
-                    eventDialogState.event.negativeAction?.let { event ->
-                        TextButton(
-                            onClick = {
-                                eventDialogState.dismiss()
-                                scope.launch {
-                                    event.action.invoke()
-                                }
-                            },
-                        ) {
-                            Text(
-                                text = event.buttonText.asString(),
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
-                        }
-                    }
-                    eventDialogState.event.positiveAction.let { event ->
-                        TextButton(
-                            onClick = {
-                                eventDialogState.dismiss()
-                                scope.launch {
-                                    event.action.invoke()
-                                }
-                            },
-                        ) {
-                            Text(
-                                text = event.buttonText.asString(),
-                                color = MaterialTheme.colorScheme.onSurface
-                            )
-                        }
-                    }
+                    Text(
+                        text = event.buttonText.asString(),
+                        color = MaterialTheme.colorScheme.inverseOnSurface
+                    )
                 }
             }
         }
@@ -131,11 +153,24 @@ fun EventAlertDialog(
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 fun EventAlertDialogPreview() {
-    val dialogState = rememberEventDialogState(initiallyVisible = true)
     PiFireTheme {
         Surface {
-            EventAlertDialog(
-                eventDialogState = dialogState
+            DialogContent(
+                eventDialogState = EventDialogState(
+                    state = rememberDialogState(),
+                    dialogEvent = DialogEvent(
+                        title = UiText(R.string.dialog_auth_with_unsecure_protocol_title),
+                        message = UiText(R.string.dialog_auth_with_unsecure_protocol_message),
+                        positiveAction = DialogAction(
+                            buttonText = UiText(R.string.continue_),
+                            action = {}
+                        ),
+                        negativeAction = DialogAction(
+                            buttonText = UiText(R.string.cancel),
+                            action = {}
+                        )
+                    )
+                )
             )
         }
     }
